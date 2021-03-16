2280d91da8d3f8b12d0042f845182a9e92aace54
Englocom Tech <claudio@englocom.com.br>
2021-03-16 20:26:27 -0300

    php examples

--- PHP-Functions.php
+++ PHP-Functions.php
@@ -46,3 +46,37 @@ function addNumbers(int $a, int $b) {
 echo addNumbers(5, "5 days");
 // since strict is enabled and "5 days" is not an integer, an error will be thrown
 ?>
+<?php
+//6.
+declare(strict_types=1); // strict requirement
+function setHeight(int $minheight = 50) {
+  echo "The height is : $minheight <br>";
+}
+
+setHeight(350);
+setHeight(); // will use the default value of 50
+setHeight(135);
+setHeight(80);
+?>
+<?php
+//7.
+declare(strict_types=1); // strict requirement
+function sum(int $x, int $y) {
+  $z = $x + $y;
+  return $z;
+}
+
+echo "5 + 10 = " . sum(5, 10) . "<br>";
+echo "7 + 13 = " . sum(7, 13) . "<br>";
+echo "2 + 4 = " . sum(2, 4);
+?>
+<?php
+//8.
+function add_five(&$value) {
+  $value += 5;
+}
+
+$num = 2;
+add_five($num);
+echo $num;
+?>
