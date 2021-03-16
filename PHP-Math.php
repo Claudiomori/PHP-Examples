abd4fd4b640c77f066de7f2d7fc21efecaeb445a
Englocom Tech <claudio@englocom.com.br>
2021-03-16 20:31:01 -0300

    php examples

---
+++ PHP-Math.php
@@ -0,0 +1,17 @@
+<?php
+//1.
+echo(pi());
+?>
+<?php
+//2.
+echo(min(0, 150, 30, 20, -8, -200));  // returns -200
+echo(max(0, 150, 30, 20, -8, -200));  // returns 150
+?>
+<?php
+//3.
+echo(abs(-6.7));  // returns 6.7
+?>
+<?php
+//4.
+echo(rand());
+?>
